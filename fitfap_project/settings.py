f928f6a290ddf6f434695a77f4f19c9d1d1e6e39
shabeer-ctp <ctpshabeersmc@gmail.com>
2025-03-02 13:35:33 +0530

    latest commit

--- fitfap_project/settings.py
+++ fitfap_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': 'postgres',      # Replace with your PostgreSQL username
         'PASSWORD': '1234',  # Replace with your PostgreSQL password
         'HOST': '0.0.0.0',          # Use 'localhost' if PostgreSQL is running locally
-        'PORT': '5432',
+        'PORT': '8000',
     }
 }
 
